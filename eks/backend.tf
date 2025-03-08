4da006b416743663a653af0d4b641643efd51b67
kaiz1994 <141048374+kaiz1994@users.noreply.github.com>
2025-03-08 17:12:30 +0530

    Update backend.tf New table eks_table

--- eks/backend.tf
+++ eks/backend.tf
@@ -10,7 +10,7 @@ terraform {
     bucket         = "d2-eks-bucket"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "eks_table"
     encrypt        = true
   }
 }
